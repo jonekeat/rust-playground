55f06ca096df236f7a5b1c6f0a60fda6b88c9af2
jonekeat <jonekeat@gmail.com>
2022-07-16 13:57:25 +0800

    remove compiled code

--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jonekeat\Documents\Rust\ Projects\rust-playground\hello_cargo\target\debug\hello_cargo.exe: C:\Users\jonekeat\Documents\Rust\ Projects\rust-playground\hello_cargo\src\main.rs
