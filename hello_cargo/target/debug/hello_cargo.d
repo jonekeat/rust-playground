f2b63ce8ad459c0ffa77f0ad432fcd1fb779b969
jonekeat <jonekeat@gmail.com>
2022-08-04 10:25:58 +0800

    add compiled code

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\jonekeat\Documents\Rust\ Projects\rust-playground\hello_cargo\target\debug\hello_cargo.exe: C:\Users\jonekeat\Documents\Rust\ Projects\rust-playground\hello_cargo\src\main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\jonekeat\Documents\Rust\ Projects\rust-playground\hello_cargo\target\release\hello_cargo.exe: C:\Users\jonekeat\Documents\Rust\ Projects\rust-playground\hello_cargo\src\main.rs
