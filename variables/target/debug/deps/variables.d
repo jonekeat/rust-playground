075cecb91b86b34894036cfd355785e85ceaa2c2
jonekeat <jonekeat@gmail.com>
2023-04-22 20:06:39 +0800

    add variables chapter examples

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\jonekeat\OneDrive\Documents\Rust Projects\rust-playground\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\jonekeat\OneDrive\Documents\Rust Projects\rust-playground\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\jonekeat\OneDrive\Documents\Rust\ Projects\rust-playground\variables\target\debug\variables.exe: C:\Users\jonekeat\OneDrive\Documents\Rust\ Projects\rust-playground\variables\src\main.rs
